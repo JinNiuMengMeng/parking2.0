38e48eb340f40cfdb40fe3228e7bdc4bcf98104c
suwanling <suwanlingswl@163.com>
2019-03-08 17:02:12 +0800

    测试ICE接口

--- ice_feature/mofity_ice/cli_ice.py
+++ ice_feature/mofity_ice/cli_ice.py
@@ -2,7 +2,6 @@
 
 import sys
 import Ice
-
 Ice.loadSlice("./stPython.ice")
 import stpy
 
@@ -40,29 +39,53 @@ def py_Send_St(communicator):
     # login
     adapter = communicator.createObjectAdapterWithEndpoints("xmrbi", "default")
     py_handle = StPy()
-
     adapter.add(py_handle, communicator.stringToIdentity("Epms_st"))
     adapter.activate()
-
     derived = stpy.st2pyPrx.uncheckedCast(adapter.createProxy(communicator.stringToIdentity("Epms_st")))
+
     res_login = recv_Barrier.login(derived, "nihao", "123")
-    print(res_login)
+    print("登录结果:", res_login)
+
+    res_heart = recv_Barrier.heartBeat()
+    print("心跳结果:", res_heart)
+
+    res_lane_rail_up = recv_Barrier.laneRailUp(100, 1)
+    print("抬杆结果:", res_lane_rail_up)
+
+    res_lane_rail_down = recv_Barrier.laneRailDown(100, 1)
+    print("落杆结果:", res_lane_rail_down)
+
+    res_config_tide_lane = recv_Barrier.configTideLane(100, 1, 0)
+    print("潮汐车道:", res_config_tide_lane)
+
+    res_upload_car_img = recv_Barrier.uploadCarImg(100, 1, 0)
+    print("车辆图片:", res_upload_car_img)
+
+    res_open_free_pass = recv_Barrier.openFreePass(100, 1)
+    print("打开免费通过:", res_open_free_pass)
+
+    res_close_free_pass = recv_Barrier.closeFreePass(100, 1)
+    print("关闭免费通过:", res_close_free_pass)
+
+    res_trigger_2nd_lpr = recv_Barrier.trigger2ndLpr(100, 1)
+    print("触发二次车牌识别:", res_trigger_2nd_lpr)
+
+    res_lane_sleep = recv_Barrier.laneSleep(100, 1)
+    print("车道休眠:", res_lane_sleep)
+
+    res_lane_wakeup = recv_Barrier.laneWakeup(100, 1)
+    print("车道唤醒:", res_lane_wakeup)
+
+    res_on_duty = recv_Barrier.onDuty(100, 1)
+    print("车道上班:", res_on_duty)
+
+    res_off_duty = recv_Barrier.offDuty(100, 1)
+    print("车道下班:", res_off_duty)
+
+    # res_logout = recv_Barrier.logout(derived, "nihao")
+    # print("退出结果:", res_logout)
 
     communicator.waitForShutdown()
-
-
-'''
-    # 抬杆
-    # 落杆
-    # 打开潮汐车道
-    # 是否长传图片
-    # 上传门号道号信息
-    # 一车一杆
-    # 自由流
-    # 车道唤醒，休眠
-    # 二次识别
-    # 上下班
-'''
 
 
 with Ice.initialize(sys.argv) as communicator:  # 初始化运行环境
--- ice_feature/mofity_ice/stPython.ice
+++ ice_feature/mofity_ice/stPython.ice
@@ -2,15 +2,16 @@ module stpy {
     interface st2py
 	{
 		
-		/*前端实现*/
+		//前端实现
+		int upLaneRecord(string dataJson, int seq);
 		int upLaneDevStat(string dataJson, int seq);
 		int upLaneWorkstat(string dataJson, int seq);
 	};
 
-	
-    interface py2st 
+
+    interface py2st
 	{
-        
+
 		//站级实现
 		int laneRailUp(int doorNo, int laneNo);
 		int laneRailDown(int doorNo, int laneNo);
@@ -23,15 +24,47 @@ module stpy {
 		int laneWakeup(int doorNo, int laneNo);
 		int onDuty(int doorNo, int laneNo);
 		int offDuty(int doorNo, int laneNo);
+
+
 		/*登陆接口
-		参数:   st2py* p 	-前端Ice代理   由登陆接口传给站级服务端 
+		参数:   st2py* p 	-python Ice代理   由登陆接口传给站级服务端
 				user	-用户名
-				passWd	-用户密码		
-		返回：   	0:成功
-			           
+				passWd	-用户密码
+		返回：   	0:成功， -1：失败
+
 		说明:	车道将本地代理通过登陆接口站级， 站级将其映射在本地， 方便调用车道接口。
 		*/
 		int login(st2py* p, string user, string passWd);
+
+
+		/*代理请求接口
+		参数:
+				doorNo	-
+				laneNo	-
+		返回：   	0:成功 -1：失败
+
+		说明:	页面将目标车场的门号道号传给站级，站级通过代理映射关系，向车道发起代理请求。
+		*/
+		int request(int doorNo, int laneNo);
+
+
+		/*登出接口
+		参数:
+				st2py* p	- 登陆时上传的代理
+				user		- 登陆用户名
+		返回：   	0:成功 -1：失败
+
+		说明:	登出接口，页面退出，站级将python代理置为不使能，停止消息推送
+		*/
+		int logout(st2py* p, string user);
+
+
+		/*心跳接口
+		参数:   缺省
+		返回：   	0:成功 -1：失败
+
+		说明:	由python客户端定时调用，维持连接心跳
+		*/
 		int heartBeat();
     };
 };
